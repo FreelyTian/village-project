723b28903a080d8d5429e6de1ae50ef0f2903ec3
FreelyTian <osk9ehdms01@gmail.com>
2025-05-01 19:04:04 -0300

    feat: Add new movement script for player

--- scripts/entitities/player/player.gd
+++ scripts/entitities/player/player.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
+@onready var animplayer = $sprite/AnimationPlayer
+@onready var animtree = $sprite/AnimationPlayer/AnimationTree
+@export var movedata : PlayerMovementData
+var _direction = Vector2.ZERO
+var _last_direction = Vector2.ZERO
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+func _ready():
+	animtree.active = true
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	move()
 	move_and_slide()
+	
+func move():
+	var _last_direction_bkp = _last_direction
+	_direction = Input.get_vector("left", "right", "up", "down")
+	_last_direction = _direction if _direction != Vector2.ZERO else _last_direction_bkp
+	if _direction != Vector2.ZERO:
+		#update_blend_pos()  Aguardando as animações
+		#set_walking(true)   Aguardando as animações
+		do_vel(movedata.accel)
+	else:
+		#set_walking(false)  Aguardando as animações
+		do_vel(movedata.fric)
+	
+		
+func do_vel(factor):
+	
+	velocity.x = lerp(velocity.x, _direction.x * movedata.SPEED, factor)
+	velocity.y = lerp(velocity.y, _direction.y * movedata.SPEED, factor)
+	
+# TO DO - implementar a animação
+#func set_walking(value):
+	#animtree.set("parameters/conditions/is_walking", value)
+	#animtree.set("parameters/conditions/idle", not value)
+	#
+#func update_blend_pos():
+	#animtree["parameters/idle/blend_position"] = _direction
+	#animtree["parameters/walk/blend_position"] = _direction
+	#
