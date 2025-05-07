5e8012b0bc9d199dcad0558d9f975303fecee459
FreelyTian <osk9ehdms01@gmail.com>
2025-05-07 20:27:23 -0300

    wip: Add player animations working, but not quite properly yet

--- scripts/entitities/player/player.gd
+++ scripts/entitities/player/player.gd
@@ -18,11 +18,11 @@ func move():
 	_direction = Input.get_vector("left", "right", "up", "down")
 	_last_direction = _direction if _direction != Vector2.ZERO else _last_direction_bkp
 	if _direction != Vector2.ZERO:
-		#update_blend_pos()  Aguardando as animações
-		#set_walking(true)   Aguardando as animações
+		update_blend_pos() # Aguardando as animações
+		set_walking(true)  # Aguardando as animações
 		do_vel(movedata.accel)
 	else:
-		#set_walking(false)  Aguardando as animações
+		set_walking(false) # Aguardando as animações
 		do_vel(movedata.fric)
 	
 		
@@ -32,11 +32,11 @@ func do_vel(factor):
 	velocity.y = lerp(velocity.y, _direction.y * movedata.SPEED, factor)
 	
 # TO DO - implementar a animação
-#func set_walking(value):
-	#animtree.set("parameters/conditions/is_walking", value)
-	#animtree.set("parameters/conditions/idle", not value)
+func set_walking(value):
+	animtree.set("parameters/conditions/is_walking", value)
+	animtree.set("parameters/conditions/idle", not value)
 	#
-#func update_blend_pos():
+func update_blend_pos():
 	#animtree["parameters/idle/blend_position"] = _direction
-	#animtree["parameters/walk/blend_position"] = _direction
+	animtree["parameters/walk/blend_position"] = _direction
 	#
